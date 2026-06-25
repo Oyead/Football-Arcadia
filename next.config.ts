b690598fda27a4f0187f9cc38618158becb70152
Oyead <eyadswailam7@gmail.com>
2026-06-25 18:27:03 +0300

    chore: update image remotePatterns for football-data.org crests

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-	/* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 	images: {
-		domains: ["lh3.googleusercontent.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "crests.football-data.org",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "*.googleusercontent.com",
+				pathname: "/**",
+			},
+		],
 	},
 };
 
